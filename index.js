5e95af596e6d29158410097cb3c92bafa878c014
praveen220704 <praveenprasath499@gmail.com>
2024-04-25 15:03:09 +0530

    connecting to the database

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+//import the mongoose module
+const mongoose=require('mongoose');
+
+console.log('Connecting to MongoDB...');
+
+//Connect to MongoDB using mongoose
+mongoose.connect(`mongodb+srv://praveenprasath499:Praveens007@cluster0.72zzvmt.mongodb.net/`)
+.then(()=>{
+    console.log('Connected to MongoDB...');
+})
+.catch((error)=>{
+    console.log('Error connecting to MongoDB...',error,message);
+})
